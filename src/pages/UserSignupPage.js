4c00b3a8eadbe2f098c0cf9c97e18026c919363d
rzinovyev <ruslan.zinovyev@hotmail.com>
2021-12-10 21:22:51 -0500

    handling input change

--- src/pages/UserSignupPage.js
+++ src/pages/UserSignupPage.js
@@ -1,21 +1,66 @@
 import React from "react";
 
 export class UserSignupPage extends React.Component {
+  state = {
+    displayName: "",
+    userName: "",
+    password: "",
+    passwordRepeat: "",
+  };
+
+  onChangeDisplayName = (event) => {
+    const value = event.target.value;
+    this.setState({ displayName: value });
+  };
+
+  onChangeUserName = (event) => {
+    const value = event.target.value;
+    this.setState({ userName: value });
+  };
+
+  onChangePassword = (event) => {
+    const value = event.target.value;
+    this.setState({ password: value });
+  };
+
+  onChangePasswordRepeat = (event) => {
+    const value = event.target.value;
+    this.setState({ passwordRepeat: value });
+  };
+
   render() {
     return (
       <div>
         <h1>Sign Up</h1>
         <div>
-          <input placeholder="Your display name" />
+          <input
+            placeholder="Your display name"
+            value={this.state.displayName}
+            onChange={this.onChangeDisplayName}
+          />
         </div>
         <div>
-          <input placeholder="Your username" />
+          <input
+            placeholder="Your username"
+            value={this.state.userName}
+            onChange={this.onChangeUserName}
+          />
         </div>
         <div>
-          <input placeholder="Your password" type="password" />
+          <input
+            placeholder="Your password"
+            value={this.state.password}
+            onChange={this.onChangePassword}
+            type="password"
+          />
         </div>
         <div>
-          <input placeholder="Repeat your password" type="password" />
+          <input
+            placeholder="Repeat your password"
+            value={this.state.passwordRepeat}
+            onChange={this.onChangePasswordRepeat}
+            type="password"
+          />
         </div>
         <div>
           <button>Sign Up</button>
