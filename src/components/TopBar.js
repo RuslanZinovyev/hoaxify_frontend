b592e18922d8ae02c5403ee4ea364b687ab9584f
rzinovyev <ruslan.zinovyev@hotmail.com>
2021-12-23 22:04:50 -0500

    changed styles for TopBar

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -6,7 +6,7 @@ import { connect } from "react-redux";
 class TopBar extends React.Component {
   render() {
     let links = (
-      <ul className="nav navbar-nav ml-auto">
+      <ul className="nav navbar-nav ms-auto">
         <li className="nav-item">
           <Link to="/signup" className="nav-link">
             Sign Up
@@ -22,7 +22,7 @@ class TopBar extends React.Component {
 
     if (this.props.user.isLoggedIn) {
       links = (
-        <ul className="nav navbar-nav ml-auto">
+        <ul className="nav navbar-nav ms-auto">
           <li className="nav-item nav-link">Logout</li>
           <li className="nav-item">
             <Link to={`/${this.props.user.username}`} className="nav-link">
@@ -36,7 +36,7 @@ class TopBar extends React.Component {
     return (
       <div className="bg-white shadow-sm mb-2">
         <div className="container">
-          <nav className="navbar navbar-light navbar-expand">
+          <nav className="navbar navbar-light navbar-expand ms-auto">
             <Link to="/" className="navbar-brand">
               <img src={logo} width="60" alt="Hoaxify" /> Hoaxify
             </Link>
