180f297f9f6d8988d808e1960531719c0dcd1aa5
rzinovyev <ruslan.zinovyev@hotmail.com>
2021-12-28 13:22:38 -0500

    connecting user sign up page to redux

--- src/index.js
+++ src/index.js
@@ -3,11 +3,9 @@ import ReactDOM from "react-dom";
 import { HashRouter } from "react-router-dom";
 import App from "./containers/App";
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
-import authReducer from "./redux/authReducer";
-import logger from "redux-logger";
+import configureStore from "./redux/configureStore";
 
-const store = createStore(authReducer, applyMiddleware(logger));
+const store = configureStore();
 
 ReactDOM.render(
   <Provider store={store}>
