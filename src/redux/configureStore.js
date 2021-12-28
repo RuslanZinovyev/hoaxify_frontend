13ae909a968bc2b58463b6a7897ccc2d43887b3a
rzinovyev <ruslan.zinovyev@hotmail.com>
2021-12-28 16:17:53 -0500

    axious authorization configuration

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -2,6 +2,7 @@ import { createStore, applyMiddleware } from "redux";
 import authReducer from "./authReducer";
 import logger from "redux-logger";
 import thunk from "redux-thunk";
+import * as apiCalls from "../api/apiCalls";
 
 const configureStore = (addLogger = true) => {
   let localStorageData = localStorage.getItem("hoax-auth");
@@ -18,6 +19,7 @@ const configureStore = (addLogger = true) => {
   if (localStorageData) {
     try {
       persistedState = JSON.parse(localStorageData);
+      apiCalls.setAuthorizationHeader(persistedState);
     } catch (error) {}
   }
 
@@ -29,6 +31,7 @@ const configureStore = (addLogger = true) => {
 
   store.subscribe(() => {
     localStorage.setItem("hoax-auth", JSON.stringify(store.getState()));
+    apiCalls.setAuthorizationHeader(store.getState());
   });
   return store;
 };
